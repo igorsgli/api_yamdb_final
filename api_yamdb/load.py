12ff3af52c455d1d013dcaf5e43c5e9abf207c03
TimurKhasanov72 <khasanov72@gmail.com>
2022-09-29 21:24:33 +0500

    add load script

---
+++ api_yamdb/load.py
@@ -0,0 +1,134 @@
+import os
+import csv
+import sqlite3
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(base_dir, "db.sqlite3")
+files_folder = os.path.join(base_dir, "static", "data")
+
+
+def load_category(cursor, dictionary_reader):
+    to_db = (
+        [(row['id'], row['name'], row['slug'])
+            for row in dictionary_reader]
+    )
+    cursor.execute("DELETE FROM reviews_category;")
+    cursor.executemany(
+        "INSERT INTO reviews_category (id, name, slug) VALUES (?, ?, ?);",
+        to_db
+    )
+
+
+def load_users(cursor, dictionary_reader):
+    to_db = ([
+        (r['id'], r['username'], r['email'], r['role'], r['bio'],
+            r['first_name'], r['last_name']) for r in dictionary_reader
+    ])
+    cursor.execute("DELETE FROM reviews_customuser;")
+    cursor.executemany(
+        """INSERT INTO reviews_customuser (
+            id, username, email, role,
+            bio, first_name, last_name
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?);""",
+        to_db
+    )
+
+
+def load_genre(cursor, dictionary_reader):
+    to_db = (
+        [
+            (row['id'], row['name'], row['slug'])
+            for row in dictionary_reader
+        ]
+    )
+    cursor.execute("DELETE FROM reviews_genre;")
+    cursor.executemany(
+        "INSERT INTO reviews_genre (id, name, slug) VALUES (?, ?, ?);",
+        to_db
+    )
+
+
+def load_titles(cursor, dictionary_reader):
+    to_db = (
+        [(row['id'], row['name'], row['year'], row['category'])
+            for row in dictionary_reader]
+    )
+    cursor.execute("DELETE FROM reviews_title;")
+    cursor.executemany(
+        """INSERT INTO reviews_title (id, name, year, category_id)
+        VALUES (?, ?, ?, ?);""",
+        to_db
+    )
+
+
+def load_genre_titles(cursor, dictionary_reader):
+    to_db = (
+        [(row['id'], row['title_id'], row['genre_id'])
+            for row in dictionary_reader]
+    )
+    cursor.execute("DELETE FROM reviews_genretitle;")
+    cursor.executemany(
+        """INSERT INTO reviews_genretitle (id, title_id, genre_id)
+            VALUES (?, ?, ?);""",
+        to_db
+    )
+
+
+def load_reviews(cursor, dictionary_reader):
+    to_db = (
+        [(row['id'], row['title_id'], row['text'], row['author'],
+            row["score"], row["pub_date"])
+            for row in dictionary_reader]
+    )
+    cursor.execute("DELETE FROM reviews_review;")
+    cursor.executemany(
+        """INSERT INTO reviews_review
+        (id, title_id, text, author_id, score, pub_date)
+        VALUES (?, ?, ?, ?, ?, ?);""",
+        to_db
+    )
+
+
+def load_comments(cursor, dictionary_reader):
+    to_db = (
+        [(row['id'], row['review_id'], row['text'], row['author'],
+            row["pub_date"])
+            for row in dictionary_reader]
+    )
+    cursor.execute("DELETE FROM reviews_comment;")
+    cursor.executemany(
+        """INSERT INTO reviews_comment
+        (id, review_id, text, author_id, pub_date)
+        VALUES (?, ?, ?, ?, ?);""",
+        to_db
+    )
+
+
+FILES = {
+    'users': load_users,
+    'category': load_category,
+    'genre': load_genre,
+    'titles': load_titles,
+    'genre_title': load_genre_titles,
+    'review': load_reviews,
+    'comments': load_comments,
+}
+
+
+if __name__ == "__main__":
+    print('connect to db:', db_path)
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+
+    for file, loader in FILES.items():
+        file_path = os.path.join(files_folder, file + ".csv")
+        with open(file_path, 'r', encoding='utf8') as f:
+            print('loading from file: {} ...'.format(file))
+            dr = csv.DictReader(f, delimiter=',')
+            loader(cursor, dr)
+            connection.commit()
+            print('success!')
+            print('')
+
+    connection.close()
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from turtle import title
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
@@ -11,47 +13,49 @@ CHOICES = (
 
 
 class CustomUser(AbstractUser):
+    password = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+    is_active = models.BooleanField(
+        blank=True,
+        null=True,
+        default=True)
+    is_staff = models.BooleanField(
+        blank=True,
+        null=True,
+        default=False
+    )
+    is_superuser = models.BooleanField(
+        blank=True,
+        null=True,
+        default=False
+    )
+    date_joined = models.DateTimeField(
+        default=datetime.now,
+        blank=True,
+        null=True
+    )
     bio = models.TextField(blank=True)
     role = models.CharField(
         max_length=15,
         choices=CHOICES,
         default='user'
     )
+    confirmation_code = models.CharField(
+        max_length=555,
+        blank=True,
+        null=True
+    )
+    token = models.CharField(
+        max_length=555,
+        blank=True,
+        null=True,
+    )
 
 
 User = get_user_model()
-
-
-class Review(models.Model):
-    text = models.TextField(help_text="Текст обзора")
-    score = models.IntegerField()
-    author = models.ForeignKey(
-        CustomUser,
-        on_delete=models.CASCADE,
-        related_name='reviews'
-    )
-    pub_date = models.DateTimeField(
-        'Дата публикации обзора',
-        auto_now_add=True
-    )
-
-
-class Comment(models.Model):
-    text = models.TextField()
-    author = models.ForeignKey(
-        CustomUser, 
-        on_delete=models.CASCADE, 
-        related_name='comments',
-        help_text="Автор"
-    )
-    pub_date = models.DateField(
-        'Дата публикации комментария', auto_now_add=True
-    )
-    review = models.ForeignKey(
-        Review, on_delete=models.CASCADE,
-        related_name='comments',
-        help_text="Обзор",
-    )
 
 
 class Category(models.Model):
@@ -73,13 +77,15 @@ class Genre(models.Model):
 class Title(models.Model):
     name = models.CharField(max_length=200)
     year = models.IntegerField()
-    description = models.CharField(max_length=200)
-    genre = models.ForeignKey(
+    description = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True
+    )
+    genre = models.ManyToManyField(
         Genre,
         blank=True,
-        null=True,
-        on_delete=models.SET_NULL,
-        related_name='titles'
+        through='GenreTitle',
     )
     category = models.ForeignKey(
         Category,
@@ -88,14 +94,61 @@ class Title(models.Model):
         on_delete=models.SET_NULL,
         related_name='titles'
     )
-    review = models.ForeignKey(
-        Review,
-        on_delete=models.SET_NULL, 
-        blank=True,
-        null=True,
-        related_name='reviews',
-        verbose_name="Обзор",
-    )
+    # review = models.ForeignKey(
+    #     Review,
+    #     on_delete=models.SET_NULL,
+    #     blank=True,
+    #     null=True,
+    #     related_name='reviews',
+    #     verbose_name="Обзор",
+    # )
 
     def __str__(self):
         return self.title
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.genre} {self.title}'
+
+
+class Review(models.Model):
+    text = models.TextField(help_text="Текст обзора")
+    score = models.IntegerField()
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        blank=True,
+        null=True,
+    )
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации обзора',
+        auto_now_add=True
+    )
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        help_text="Автор"
+    )
+    pub_date = models.DateField(
+        'Дата публикации комментария', auto_now_add=True
+    )
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE,
+        related_name='comments',
+        help_text="Обзор",
+    )
