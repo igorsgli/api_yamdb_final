8c26a7167c9d2da4113ba5cbbd70dec9e0194de4
TimurKhasanov72 <khasanov72@gmail.com>
2022-09-29 22:02:14 +0500

    urls: fix bug in token

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -13,7 +13,7 @@ v1_router.register('titles', TitleViewSet, basename='titles')
 
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('v1/', include(v1_router.urls)),
 ]
